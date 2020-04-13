6182b9ee5fcf8ae8e4a4483d1dca3e0a4759972b
aininkh <aini.khasanah@students.amikom.ac.id>
2020-04-13 15:10:57 +0700

    Responsi Mid

---
+++ Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2595
+{
+    public class Karyawan
+    {
+        public int No { get; set; }
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public double GajiBulanan { get; set; }
+
+        public Karyawan(int _No, string _NIK, string _Nama, int _GajiBulanan)
+        {
+
+            No = _No;
+            NIK = _NIK;
+            Nama = _Nama;
+            GajiBulanan = _GajiBulanan;
+
+            if(GajiBulanan<0)
+            {
+                Console.WriteLine("Gaji tidak boleh kurang dari 0");
+                GajiBulanan = 0;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman2595
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Karyawan karyawan1 = new Karyawan(1, "190302213", "Aini", 3000000);
+            Karyawan karyawan2 = new Karyawan(2, "1903021224", "Aquilae", 2000000);
+
+            Console.WriteLine("No Nik/Nama\tGaji Bulanan");
+            Console.WriteLine("---------------------------");
+            CetakGaji(karyawan1);
+            CetakGaji(karyawan2);
+
+            Console.WriteLine("\nKenaikan Gaji 10%");
+            Console.WriteLine("No NIK/Nama\tGaji Bulanan");
+            Console.WriteLine("---------------------------");
+            NaikGaji(karyawan1);
+            NaikGaji(karyawan2);
+            Console.ReadKey();
+        }
+
+        static void CetakGaji(Karyawan pegawai)
+        {
+            Console.WriteLine(pegawai.No + ". "+ pegawai.NIK + " " + pegawai.Nama + " " + pegawai.GajiBulanan);
+        }
+
+        static void NaikGaji(Karyawan pegawai)
+        {
+            double kenaikan;
+            kenaikan = pegawai.GajiBulanan * 0.1;
+            pegawai.GajiBulanan = pegawai.GajiBulanan + kenaikan;
+            Console.WriteLine(pegawai.No + ". " + pegawai.NIK + " " + pegawai.Nama + " " + pegawai.GajiBulanan);
+        }
+    }
+}
